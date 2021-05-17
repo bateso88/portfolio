7c914b8e19eb158aaecc7fd5739eff2d69e31d79
bateso88 <bateso@hotmail.co.uk>
2021-05-17 16:38:05 +0100

    Restyle buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,26 +6,24 @@ const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
-export const Button =({
+export const Button = ({
   children,
   type,
   onClick,
-  buttonStyle,buttonSize
+  buttonStyle,
+  buttonSize
 }) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-  ? buttonStyle
-  : STYLES[0];
+  const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
-  const checkButtonSize = SIZES.includes(buttonSize)
-  ? buttonSize
-  :SIZES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
-    <Link to='sign-up' className='btn-mobile'>
+    <Link to='/sign-up' className='btn-mobile'>
       <button 
-      clasName={`btn ${checkButtonStyle} ${checkButtonSize}`}
-      onClick={onClick}
-      type={type}>
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
         {children}
       </button>
     </Link>
