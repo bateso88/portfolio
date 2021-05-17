6553f62a2fec43e2fee0ed988b2347ca37e112e9
bateso88 <bateso@hotmail.co.uk>
2021-05-17 21:21:35 +0100

    Add styling to hero section

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component=
-          {Home}/>
+          <Route path='/' exact component={Home}/>
         </Switch>
       </Router>
     </>
