6a2274e51d7e80259e58a903b42feb1133f1470d
bateso88 <bateso@hotmail.co.uk>
2021-05-22 12:36:59 +0100

    Tidy files

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -3,21 +3,57 @@ export const SliderData = [
     image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_0215.jpeg"
   },
   {
-    image: "images/IMG_0263.jpeg"
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_0263.jpeg"
   },
   {
-    image: "images/IMG_0517.jpeg"
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_0346.jpeg"
   },
   {
-    image: "images/IMG_0823.jpeg"
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_0375.jpeg"
   },
   {
-    image: "images/IMG_1690.jpeg"
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_0517.jpeg"
   },
   {
-    image: "images/IMG_1752.jpeg"
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_0823.jpeg"
   },
   {
-    image: "images/IMG_4653.jpeg"
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1115.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1212.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1283.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1339.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1387.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1537.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1690.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1752.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1938.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_1956.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_2188.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_2360.jpeg"
+  },
+  {
+    image: "https://oliverbateswebsite.s3.amazonaws.com/IMG_2506.jpeg"
   }
 ]
